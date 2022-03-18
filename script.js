5841c1f98d04cd043b0dedf3ed2e2790edbbff50
eliina13 <92846329+eliina13@users.noreply.github.com>
2022-03-18 11:47:13 +0200

    n

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log(2)
+//console.log(3)
+let zina = document.querySelector('.manaZina');
+let zinas= document.querySelector('.chataZinas');
+
+function sutittZinu(){
+    console.log('Sutiīt ziņu()darbojas');
+   zinas.innerHTML = zinas.innerHTML + '<br/>'+ zina.value ;
+}
+
+async function ieladetChataZinas(){
+    let datiNoServera = await fetch('catazinas.txt');
+    let dati =await datiNoServera.text();
+    zinas.innerHTML = dati;
+}
