73d770edad9fbf3f3eab7447764d77ee627a408f
eliina13 <92846329+eliina13@users.noreply.github.com>
2022-03-18 15:20:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const API = 'https://cataaplikacija.eliina13.repl.co'
 
 let zina = document.querySelector('.manaZina');
 let zinas= document.querySelector('.chataZinas');
+let vards = document.querySelector ('.vards');
 
 function sutittZinu(){
     console.log('Sutiīt ziņu()darbojas');
    zinas.innerHTML = zinas.innerHTML + '<br/>'+ zina.value ;
    
-fetch(API + '/sutit/Juris/'+ zina.value)
+fetch(API + '/sutit/'+vards.value'/'+ zina.value)
 }
 
 async function ieladetChataZinas(){
